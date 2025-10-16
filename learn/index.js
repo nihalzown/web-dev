33bb92cefd4f9d41d90456aa2d337a5933d8cf85
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2025-10-16 20:48:23 +0530

    user input

--- learn/index.js
+++ learn/index.js
@@ -1,18 +1,5 @@
-//console.log(`Hello`);
-//console.log(`i like pizza`);
-
-//window.alert(`this is an alert`);
-//window.alert(`i like pizza`);
-
-
-document.getElementById("myh1").textContent=`hello`;
-document.getElementById("myp").textContent=`i like pizza`;
-
-let age = 25;
-let price = 10.99;
-let gpa = 2.1;
-
-console.log(typeof gpa)
-console.log(`you are ${age} years old`)
-
-// this is a comment
+let username;
+document.getElementById("mysubmit").onclick = function() {
+    username = document.getElementById("mytext").value;
+    document.getElementById("myh1").textContent = `hello ${username}`;
+}
