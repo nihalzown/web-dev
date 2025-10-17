9ba03832da6c76c0446611831b6d75944503ae8d
MUHAMMED NIHAL <145740029+nihalzown@users.noreply.github.com>
2025-10-17 22:35:45 +0530

    circumference

--- learn/index.js
+++ learn/index.js
@@ -1,5 +1,11 @@
-let username;
+const PI = 3.14159;
+let circumference;
+let radius;
+
+
 document.getElementById("mysubmit").onclick = function() {
-    username = document.getElementById("mytext").value;
-    document.getElementById("myh1").textContent = `hello ${username}`;
+    radius = document.getElementById("mytext").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("result").textContent = "Circumference is " + circumference + " units. ";
 }
